1e83360081fa9ecca114088fe8ec8b78171dd728
John Diczhazy <jbdkz100@gmail.com>
2016-11-22 15:16:26 -0500

    Create Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-    get '/events' => 'events#index'
- 
+   get '/' => 'events#home' 
+   get '/index' => 'events#index'
+    
 
 
   # The priority is based upon order of creation: first created -> highest priority.
